c6ffb221e0132e43d7db81d1c1122282271196d0
jafow <jared.a.fowler@gmail.com>
2017-05-06 22:52:06 -0700

    Add module

---
+++ index.js
@@ -0,0 +1,61 @@
+const net = require('net')
+const fs = require('fs')
+const path = require('path')
+
+module.exports = WFTS
+
+function WFTS (options) {
+  this.PORT = options.port || 4000
+  this.host = options.host || 'localhost'
+  this.file = toPath(options.file)
+  this.cmd = options.cmd
+}
+
+WFTS.prototype.serve = function serve () {
+  const server = net.createServer((socket) => {
+    socket.on('end', close)
+    socket.on('error', (err) => {
+      throw err
+    })
+
+    fs.createReadStream(this.file).pipe(socket)
+  })
+
+  server.on('error', (err) => {
+    console.error(err)
+    process.exit(1)
+  })
+
+  server.on('close', close)
+  server.listen(this.PORT)
+}
+
+WFTS.prototype.pull = function pull () {
+  const fsw = fs.createWriteStream(this.file, {flags: 'w', defaultEncoding: 'utf8', autoClose: true})
+
+  const socket = net.connect(this.PORT, HOST, () => {
+    socket.pipe(fsw).on('finished', () => { console.log('hooo')})
+  })
+
+  socket.on('end', () => {
+    console.log('transfer complete')
+    socket.destroy()
+    socket.end()
+  })
+
+  socket.on('error', (err) => {
+    console.error(err)
+    process.exit(1)
+  })
+}
+
+function toPath(p) {
+  if (!/\/d+/.test(p)) return path.join(__dirname, p)
+  return p
+}
+
+function close () {
+  console.log('client disconnected')
+  process.exit(0)
+}
+
