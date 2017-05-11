ed4bc73ed7a9024f7c363ee90a4aadcee870f5af
jafow <jared.a.fowler@gmail.com>
2017-05-11 13:57:13 -0700

    Clean up files from test; lint

--- test/test.js
+++ test/test.js
@@ -1,24 +1,27 @@
 const test = require('tape')
-const ram = require('random-access-memory')
 const fs = require('fs')
-const wfts = require('../')
+const Wfts = require('../')
 
 test('sends file', function (t) {
   t.plan(4)
-  var server = new wfts({port: 3000, file: 'test/hello.txt'})
-  var client = new wfts({port: 3000, file: 'test/world.txt'})
+  var server = new Wfts({port: 3000, file: 'test/hello.txt'})
+  var client = new Wfts({port: 3000, file: 'test/world.txt'})
   server.serve()
-  client.pull(pullCallback)
-
-  function pullCallback () {
+  client.pull(function () {
     fs.lstat('test/world.txt', function (err, stat) {
       t.equal(err, null)
-        t.ok(stat.isFile(), 'makes a file')
+      t.ok(stat.isFile(), 'makes a file')
     })
 
-    fs.readFile('./test/world.txt', function (err, data) {
+    fs.readFile('test/world.txt', function (err, data) {
       t.equal(err, null)
       t.same(data.toString(), 'hello world\n', 'target data matches source')
     })
-  }
+  })
+})
+
+test.onFinish(function () {
+  fs.unlink('test/world.txt', function (err) {
+    if (err) throw err
+  })
 })
