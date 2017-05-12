18034885e67c312e62e0ecbd1b6ceebc8f66d27a
jafow <jared.a.fowler@gmail.com>
2017-05-12 14:08:25 -0700

    Update cli with help output

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 
-const wfts = require('../')
-const cmd = process.argv[2]
-const opts = process.argv.slice(3)
+const WFTS = require('../')
+const argv = require('minimist')(process.argv.slice(2))
+
+const command = argv._[0];
+const options = {
+  file: argv._[1],
+  port: argv.p,
+  host: argv.h,
+}
+if (argv.help) {
+  console.log('usage: wfts <command> [file] [-p PORT] [-h host] [--help] \n')
+  console.log('example: \n')
+  console.log('Serve a file on default host and port (localhost:4000)\n')
+  console.log('\t$ wfts serve my-file.txt\n')
+  console.log('Receive a file on host 10.1.2.3 and port 3333\n')
+  console.log('\t$ wfts pull my-received-file.txt -h 10.1.2.3 -p 3333\n')
+}
+new WFTS(options)[command]()
